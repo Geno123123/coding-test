34b5537eda746f4e5169f095d6e6829fb5bbad89
Geno123123 <leesj070e@gmail.com>
2026-07-30 15:31:57 +0900

    refactor: modify maxmin solution

--- Basic/maxmin.py
+++ Basic/maxmin.py
@@ -7,18 +7,5 @@
 def solution(s):
     ar=list(map(int,s.split()));
     return str(min(ar))+" "+str(max(ar));
-    return maxN
-    # ar = s.split(' ');
-    # arr = []
-    # for i in ar:
-    #     arr.append(int(i));
-    # maxN = str(max(arr))
-    # minN = str(min(arr))
-    # strR=""
-    # strR+=minN
-    # strR+=" "
-    # strR+=maxN
-    # return strR
-
 
 print(solution("-1 -2 -3 -4"));

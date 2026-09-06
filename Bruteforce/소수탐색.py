cc2e1b7ff022794480176f1051c2730e3fe21c6c
Geno123123 <leesj070e@gmail.com>
2026-09-06 21:20:01 +0900

    solve: Bruteforce 소수 탐색 문제

--- Bruteforce/소수탐색.py
+++ Bruteforce/소수탐색.py
@@ -7,24 +7,22 @@
 
 from itertools import permutations
 def isPrime(num):
-    if num==1 or num==0:
+    if num<2:
         return 0
-    for i in range(2,num):
+    for i in range(2,int(num**0.5)+1):
         if num%i==0:
             return 0
     return 1
 
 def solution(numbers):
     arr=[]
-    answer = []
+    answer = set()
     for number in numbers:
         arr.append(number)
     for i in range(1,len(arr)+1):
         p=permutations(arr,i)
         for x in p:
             num=''.join(x)
-            # print(num)
             if isPrime(int(num))==1:
-                if int(num) not in answer:
-                    answer.append(int(num))
+                answer.add(int(num))
     return len(answer)
